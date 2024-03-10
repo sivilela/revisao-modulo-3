6284c70b9c5898bccb93845280d2c43c42983b05
sivilela <sivilela.s@gmail.com>
2024-03-09 22:34:48 -0300

    Criado as tipagens dos DTOs de cada endpoint

---
+++ src/dtos/playlist/likeOrDislikePlaylist.dto.ts
@@ -0,0 +1,17 @@
+likeOrDislikePlaylist.dto
+
+import z from 'zod'
+
+export interface LikeOrDislikePlaylistInputDTO {
+  playlistId: string,
+  token: string,
+  like: boolean
+}
+
+export type LikeOrDislikePlaylistOutputDTO = undefined
+
+export const LikeOrDislikePlaylistSchema = z.object({
+  playlistId: z.string().min(1),
+  token: z.string().min(1),
+  like: z.boolean()
+})
